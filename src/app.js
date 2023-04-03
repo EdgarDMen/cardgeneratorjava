88a8ff9be88c5a30f3e0096957386f21ca7f8f82
EdgarDMen <musicalminor@gmail.com>
2023-04-03 17:58:58 +0000

    completed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,60 @@
 /* eslint-disable */
+"use strict";
+import { eventListeners } from "@popperjs/core";
 import "bootstrap";
+import { Button } from "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+document.addEventListener("DOMContentLoaded", function() {
+  const generateBtn = document.querySelector(".btn");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  generateBtn.addEventListener("click", function() {
+    generateRandomCard();
+  });
+
+  generateRandomCard();
+});
+
+function generateRandomCard() {
+  const suits = [
+    { name: "Clubs", symbol: "♣" },
+    { name: "Diamonds", symbol: "♦" },
+    { name: "Hearts", symbol: "♥" },
+    { name: "Spades", symbol: "♠" }
+  ];
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+
+  const cardElement = document.getElementById("randomCard");
+  const topLeftSuitElement = cardElement.querySelector(".top-left");
+  const bottomRightSuitElement = cardElement.querySelector(".bottom-right");
+  const valueElement = cardElement.querySelector(".value");
+
+  topLeftSuitElement.textContent = randomSuit.symbol;
+  topLeftSuitElement.style.color =
+    randomSuit.name === "Diamonds" || randomSuit.name === "Hearts"
+      ? "red"
+      : "black";
+  bottomRightSuitElement.textContent = randomSuit.symbol;
+  bottomRightSuitElement.style.color =
+    randomSuit.name === "Diamonds" || randomSuit.name === "Hearts"
+      ? "red"
+      : "black";
+  valueElement.textContent = randomValue;
+}
